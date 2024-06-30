b96a4fd241138c98625ec4799ceb8165d5908293
ABHIRAJ T R <abhirajtr007@gmail.com>
2024-06-30 22:55:16 +0530

    removed image

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -98,14 +98,14 @@ const Link = () => {
 
                     songData.downloadUrl.map((song, index) => (
                         <div key={index} className="border">
-                            <img src={songData.image[2].url} className="aspect-square w-50 rounded-md" alt="" />
+                            {/* <img src={songData.image[2].url} className="aspect-square w-full md:w-52  rounded-md" alt="" /> */}
                             <div className="min-h-min p-3">
                                 <p className="mt-2 w-full text-xs font-semibold leading-tight text-gray-700">
-                                    #{songData.language}
+                                    Language: {songData.language}
                                 </p>
-                                <p className="mt-2 flex-1 text-base font-semibold text-gray-900">{songData.name}</p>
+                                <p className="mt-2 flex-1 text-base font-semibold text-gray-900">Name: {songData.name}</p>
                                 <p className="mt-2 w-full text-sm leading-normal text-gray-600">
-                                    {song.quality}
+                                    Quality: {song.quality}
                                 </p>
                                 <div className="mt-2 flex space-x-3 ">
                                     {/* {console.log(song.url)} */}
